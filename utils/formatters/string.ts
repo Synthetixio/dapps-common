5f893d76732e25e8f5f65ae30448cf5988a99d1e
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-11-02 12:56:29 +1100

    switch to relative paths + add useLocalStorage hook

--- utils/formatters/string.ts
+++ utils/formatters/string.ts
@@ -1,4 +1,4 @@
-import { CurrencyKey } from 'constants/currency';
+import { CurrencyKey } from '../../constants/currency';
 
 export const truncateAddress = (address: string, first = 5, last = 5) =>
 	`${address.slice(0, first)}...${address.slice(-last, address.length)}`;
